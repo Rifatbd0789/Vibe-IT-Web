75b0eb6807a3f485a637373392867200beb12eaf
Rifat Rabbi <rifayrabby78@gmail.com>
2023-12-13 12:15:28 +0600

    added photo on the private dashboard

--- src/Components/Shared/DashHeaderTitle.jsx
+++ src/Components/Shared/DashHeaderTitle.jsx
@@ -7,22 +7,28 @@ const DashHeaderTitle = () => {
   return (
     <div className="">
       <HeaderTitle title={`Home (${Role?.user})`} />
-      <h1 className=" text-center mx-20 mb-10 text-3xl  ">
-        Hi, Welcome {user?.user?.displayName ? user?.user?.displayName : "Back"}
-      </h1>
-      <div className="text-center text-2xl space-y-2">
-        <p>
-          <span className="underline font-medium">Name:</span>{" "}
-          <span>{user?.user?.displayName}</span>
-        </p>
-        <p>
-          <span className="underline font-medium">Email:</span>{" "}
-          {user?.user?.email}
-        </p>
-        <p>
-          {" "}
-          <span className="underline font-medium">Role:</span> {Role?.user}
-        </p>
+      <div className="flex justify-center items-center">
+        <img className="w-64 rounded-xl" src={user?.user?.photoURL} alt="" />
+        <div>
+          <h1 className=" text-center mx-20 mb-10 text-3xl  ">
+            Hi, Welcome{" "}
+            {user?.user?.displayName ? user?.user?.displayName : "Back"}
+          </h1>
+          <div className="text-center text-2xl space-y-2">
+            <p>
+              <span className="underline font-medium">Name:</span>{" "}
+              <span>{user?.user?.displayName}</span>
+            </p>
+            <p>
+              <span className="underline font-medium">Email:</span>{" "}
+              {user?.user?.email}
+            </p>
+            <p>
+              {" "}
+              <span className="underline font-medium">Role:</span> {Role?.user}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
